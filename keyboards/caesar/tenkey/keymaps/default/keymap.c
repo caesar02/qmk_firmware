1fd32ed3007cf00fc744b2adaea6d855505ea6be
caesar02 <caesar02@mac.com>
2026-02-04 23:33:26 +0900

    Update keymap.c

--- keyboards/caesar/tenkey/keymaps/default/keymap.c
+++ keyboards/caesar/tenkey/keymaps/default/keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,  KC_P7,   KC_P8,   KC_P9,   KC_BSPC,
         KC_TAB,  KC_P4,   KC_P5,   KC_P6,   KC_PENT,
-        KC_LCTL, KC_P1,   KC_P2,   KC_P3,   KC_MIC_MUTE,
+        KC_LCTL, KC_P1,   KC_P2,   KC_P3,   DF(0),
         KC_LSFT, KC_P0,   KC_SPC
     )
 };
